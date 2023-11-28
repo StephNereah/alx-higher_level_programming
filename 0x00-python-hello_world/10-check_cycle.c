ee5256577536a015434413b197b5f1bc8a7b79f2
StephNereah <stphnadhiambo@gmail.com>
2023-11-27 23:50:19 -0800

    task 10

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,27 +1,26 @@
 #include "lists.h"
 
 /**
- *check_cycle - checks if a singly linked list has a cycle in it
- *list - linked list
+ * check_cycle - checks if a singly linked list has a cycle in it
+ * list - linked list
  *
- *Return: 0 if cycle, 1 if no cycle
- *
+ * Return: 0 if cycle, 1 if no cycle
  */
 
 int check_cycle(listint_t *list)
 {
-	listint_t *mom, *dad;
+	listint_t *tortoise, *hare;
 
-	mom = list;
-	dad = list;
+	tortoise = list;
+	hare = list;
 
-	while (mom && dad)
+	while (tortoise && hare)
 	{
-		if (dad->next == NULL)
+		if (hare->next == NULL)
 			return (0);
-		mom = mom->next;
-		dad = dad->next;
-		if (mom == dad)
+		tortoise = tortoise->next;
+		hare = hare->next->next;
+		if (tortoise == hare)
 			return (1);
 	}
 

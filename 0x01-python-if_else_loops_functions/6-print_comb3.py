ff47666bd841ca623d926679cb3991ab17fe90eb
StephNereah <stphnadhiambo@gmail.com>
2024-02-07 02:56:16 -0800

    print_comb3

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,4 +2,4 @@
 
 for first in range(10):
     for second in range(first + 1, 10):
-        print("{}{}".format(first, second), end=", " if first != 8 or second != 9 else "\n")
+        print("{}{}".format(first, second), end=", " if (first != 8 or second != 9) else "\n")

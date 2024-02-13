870df9a00507f15e47500106a3ec14c570476062
StephNereah <stphnadhiambo@gmail.com>
2024-02-12 17:14:47 -0800

    task 5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
+
 const uwu = parseInt(process.argv[2]);
-if (isNaN(uwu)) {
-	console.log('Not a number');
+if (Number.isNaN(uwu)) {
+  console.log('Not a number');
 } else {
-	console.log('My number: ' + uwu);
+  console.log('My number: ' + uwu);
 }

6f2159e1d9403346e946f19234774fb25b76143b
StephNereah <stphnadhiambo@gmail.com>
2024-02-15 18:23:28 -0800

    task 7

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,10 +1,1 @@
-#!/usr/bin/node
-exports.nbOccurences = function (list, searchElement) {
-  let i = 0;
-  list.forEach(function (obj) {
-    if (obj === searchElement) {
-      i++;
-    }
-  });
-  return i;
-};
+

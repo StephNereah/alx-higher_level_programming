49236e231bfd9d78e699413efedbf199b2b78dbc
StephNereah <stphnadhiambo@gmail.com>
2024-03-13 17:52:41 -0700

    interview prep

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,21 +1,39 @@
 #!/usr/bin/python3
 
 def roman_to_int(roman_string):
-    if not isinstance(roman_string, str) or not roman_string:
+    if not isinstance(roman_string, str):
         return 0
 
-
-    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50,'C': 100, 'D': 500, 'M': 1000}
-
+    num = 0
     res = 0
-    prev_value = 0
-    for x in reversed(roman_string):
-        value = roman[x]
-        if value < prev_value:
-            res -= value 
-        else:
-            res += value
-        
-        prev_value = value
+    aux = 0
+    list_num = []
+
+    for s in roman_string:
+        if s == 'I':
+            num = 1
+        elif s == 'V':
+            num = 5
+        elif s == 'X':
+            num = 10
+        elif s == 'L':
+            num = 50
+        elif s == 'C':
+            num = 100
+        elif s == 'D':
+            num = 500
+        elif s == 'M':
+            num = 1000
+        list_num.append(num)
+
+    len_list = len(list_num)
+    if len_list > 1:
+        for i in range(1, len_list):
+            if list_num[i] > list_num[i - 1]:
+                    aux = list_num[i] - list_num[i - 1]
+                    list_num[i] = aux
+                    list_num[i - 1] = 0
+    for num in list_num:
+        res += num
 
     return res

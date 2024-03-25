377c800d18b3f2973dacf7a54b97332463c25e3d
StephNereah <stphnadhiambo@gmail.com>
2024-03-25 04:01:14 -0700

    2. Say my name

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 
-"""This module writtes a function that prints My name is <first name> <last name>."""
+"""This module is a function that prints My name is <first name> <last name>"""
 
 
 def say_my_name(first_name, last_name=""):
-    if type(first_name) is not str:
+    """
+    prints a hello string for a name, handling input errors
+    """
+    if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
-
-    if type(last_name) is not str:
+    if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
-
     print("My name is {} {}".format(first_name, last_name))

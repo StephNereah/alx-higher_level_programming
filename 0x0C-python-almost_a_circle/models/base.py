11b58e8cd4b289babf2df86cb145cd03322a16e0
StephNereah <stphnadhiambo@gmail.com>
2024-05-11 13:22:23 -0700

    1.Base Class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
 
 class Base:
-    """ATTRIBUTES:
-    private class attribute: __nb_objects = 0
-    class constructor: def __init__(self, id=None)
+    """Base class
+    Attributes:
+        __nb_objects: private class attribure
+        id (int): instance public attribute
     """
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """
-        public instance attribute: id(int)
+        """class constructor
+        Args:
+            id (int): instance public attribute
         """
         if id is not None:
             self.id = id
         else:
-            Base.__nb_objects =+ 1
+            Base.__nb_objects += 1
             self.id = Base.__nb_objects

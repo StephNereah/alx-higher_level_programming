95853f0a83de5fd42bc398b0955a050e98aadc8a
StephNereah <stphnadhiambo@gmail.com>
2024-05-23 07:16:58 -0700

    fresh start

--- 0x12-javascript-warm_up/10-factorial.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-function factorial (babes) {
-  if ((Number.isNaN(babes)) || (babes === 1)) {
-    return 1;
-  }
-  return factorial(babes - 1) * babes;
-}
-
-console.log(factorial(parseInt(process.argv[2])));
--- 0x12-javascript-warm_up/5-to_integer.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/node
-
-const uwu = parseInt(process.argv[2]);
-if (Number.isNaN(uwu)) {
-  console.log('Not a number');
-} else {
-  console.log('My number: ' + uwu);
-}

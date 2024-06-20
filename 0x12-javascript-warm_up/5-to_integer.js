1dc19abd6f7d8d18a69165460df7e5e7ce7c343b
StephNereah <stphnadhiambo@gmail.com>
2024-06-20 14:34:51 -0700

    forgot spaces

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -5,7 +5,7 @@ const args = process.argv.slice(2);
 const number = parseInt(args[0], 10);
 
 if (!isNaN(number)) {
-  console.log('My number:' + number);
+  console.log('My number: ' + number);
 } else {
   console.log('Not a number');
 }

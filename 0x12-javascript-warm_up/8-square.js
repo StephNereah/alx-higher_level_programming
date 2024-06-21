3e602fff843442a7a3b676314d88517f82a940f0
StephNereah <stphnadhiambo@gmail.com>
2024-06-21 03:35:37 -0700

    8. Square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const args = process.argv.slice(2);
+
+const size = parseInt(args[0], 10);
+
+if (isNaN(size) || size <= 0) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < size; i++) {
+    console.log('X'.repeat(size));
+  }
+}

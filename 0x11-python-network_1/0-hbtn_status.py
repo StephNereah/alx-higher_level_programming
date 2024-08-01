eb200e6a34044eb274fc5eaf6fd6b693623c98e5
StephNereah <stphnadhiambo@gmail.com>
2024-07-31 19:59:57 -0700

    Python script that fetches https://alx-intranet.hbtn.io/status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
 """Module that fetches https://alx-intranet.hbtn.io/status"""
-
 import urllib.request
 
-url = 'https://alx-intranet.hbtn.io/status' #Define url
 
-#Open and fetch response from URL
 if __name__ == "__main__":
-    url = 'https://alx-intranet.hbtn.io/status' #Define url
+    url = "https://alx-intranet.hbtn.io/status"
     with urllib.request.urlopen(url) as response:
-        body = response.read()
-        print("Body response:")
-        print(f"    - type: {type(body)}")
-        print(f"    - content: {body}")
-        print(f"    - utf8 content: {body.decode('utf-8')}")
+        html = response.read()
+        print('Body response:')
+        print('\t- type: {}'.format(type(html)))
+        print('\t- content: {}'.format(html))
+        print('\t- utf8 content: {}'.format(html.decode("utf-8")))

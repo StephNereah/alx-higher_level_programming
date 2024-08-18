34cea846f7491c97064c8792ada4f3586de0b70f
StephNereah <stphnadhiambo@gmail.com>
2024-08-18 16:20:03 -0700

    trial

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
-const list = require('./100-data').list;
-const newList = list.map(function (num, index) {
-  return num * index;
-});
+const initList = require('./100-data.js').list;
+const newList = initList.map((number, index) => number * index);
 
-console.log(list);
+console.log(initList);
 console.log(newList);
